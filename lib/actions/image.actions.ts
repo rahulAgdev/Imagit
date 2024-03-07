8610e04963473f20732a53a56ef7f56588b1c573
Rahul Agarwal <rahulagdev@gmail.com>
2024-03-07 11:21:18 +0530

    Adding serching feature and pagination feature

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -6,8 +6,9 @@ import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
-
-const populateUser = (query : any) =>query.populate({
+import {v2 as cloudinary} from 'cloudinary'
+import { AnyArray } from "mongoose";
+const populateUser = (query : any) => query.populate({
     path: 'author',
     model: User,
     select: '_id firstName lastName'
@@ -80,6 +81,64 @@ export async function getImageById(imageId: string) {
     if(!image) throw new Error("Image not found");
     // allows us to show the new image rather than just the cache image
     return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// getting all posts 
+
+export async function getAllImages({limit = 9 , page = 1, searchQuery = ''} : {
+  limit?: number;
+  page: number;
+  searchQuery?: string
+}) { 
+  try {
+
+    await connectToDatabase();
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: true,
+
+    })
+    
+    let expression = 'folder=imagit';
+
+    if(searchQuery){
+      expression += `AND ${searchQuery}`
+    }
+
+    const {resources} =  await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource : any)=>resource.public_id);
+
+    let query = {};
+    if(searchQuery){
+      query={
+        publicId: {
+          $in: resourceIds
+        }
+      }
+    }
+
+    const skipAmount = (Number(page)-1)*limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({updatedAt: -1})
+      .skip(skipAmount)
+      .limit(limit)
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+    return {
+      data : JSON.parse(JSON.stringify(images)),
+      totalPage : Math.ceil(totalImages/limit),
+      savedImages
+    }
   } catch (error) {
     handleError(error);
   }
